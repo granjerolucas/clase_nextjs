4274e00ad722a025e4d5f69592eaa310106df932
granjerolucas <56519538+granjerolucas@users.noreply.github.com>
2024-10-14 11:49:19 -0500

    container layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,27 +21,29 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-black p-10">
-        <div className="bg-slate-900 rounded-[3rem] p-5">
-          <div className="text-white">
-            <div className="grid grid-cols-2   absolutex w-fullx  m-4 ">
-              <div className="max-w-xsx text-left">
-                <p>Longitude - 28.12312</p>
-                <p>Latitude - 28.12312</p>
+        <div className="container mx-auto">
+          <div className="bg-slate-900 rounded-[3rem] p-5">
+            <div className="text-white">
+              <div className="grid grid-cols-2   absolutex w-fullx  m-4 ">
+                <div className="max-w-xsx text-left">
+                  <p>Longitude - 28.12312</p>
+                  <p>Latitude - 28.12312</p>
+                </div>
+                <div className="max-w-xsx text-right">
+                  <p>Longitude - 28.12312</p>
+                  <p>Latitude - 28.12312</p>
+                </div>
               </div>
-              <div className="max-w-xsx text-right">
-                <p>Longitude - 28.12312</p>
-                <p>Latitude - 28.12312</p>
-              </div>
-            </div>
-            <div className="container mx-auto ">{children}</div>
-            <div className="grid grid-cols-2   absolutex w-fullx  m-4 ">
-              <div className="max-w-xsx text-left">
-                <p>Longitude - 28.12312</p>
-                <p>Latitude - 28.12312</p>
-              </div>
-              <div className="max-w-xsx text-right">
-                <p>Longitude - 28.12312</p>
-                <p>Latitude - 28.12312</p>
+              <div className="px-20 ">{children}</div>
+              <div className="grid grid-cols-2   absolutex w-fullx  m-4 ">
+                <div className="max-w-xsx text-left">
+                  <p>Longitude - 28.12312</p>
+                  <p>Latitude - 28.12312</p>
+                </div>
+                <div className="max-w-xsx text-right">
+                  <p>Longitude - 28.12312</p>
+                  <p>Latitude - 28.12312</p>
+                </div>
               </div>
             </div>
           </div>
